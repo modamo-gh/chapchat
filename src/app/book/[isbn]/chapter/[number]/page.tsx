71dee47a6be89f060ef6af38d4dc82b406b7e468
Bryan <morganthemosaic@gmail.com>
2025-09-14 11:53:17 -0400

    Add to chapter page skeleton

--- src/app/book/[isbn]/chapter/[number]/page.tsx
+++ src/app/book/[isbn]/chapter/[number]/page.tsx
@@ -7,7 +7,10 @@ const ChapterPage = () => {
 			<Header />
 			<div className="col-span-10 gap-4 grid grid-cols-5 grid-rows-5 h-full row-span-8">
 				<div className="bg-[#87A96B] border-zinc-800 border-3 col-span-1 gap-2 grid grid-cols-1 grid-rows-5 p-2 rounded-lg row-span-5"></div>
-				<div className="border-zinc-800 border-3 col-span-4 gap-2 grid grid-cols-4 grid-rows-4 rounded-lg row-span-5"></div>
+				<div className="col-span-4 gap-4 grid grid-cols-4 grid-rows-4 rounded-lg row-span-5">
+					<div className="bg-[#87A96B] border-zinc-800 border-3 col-span-5 p-2 rounded-lg row-span-1"></div>
+					<div className="bg-[#87A96B] border-zinc-800 border-3 col-span-5 p-2 rounded-lg row-span-4"></div>
+				</div>
 			</div>
 			<Footer />
 		</div>

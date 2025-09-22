29255f0506c929ce20fc33ee640c780874930b65
modamo-gh <morganthemosaic@gmail.com>
2025-09-22 11:16:50 -0400

    Change button color on hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ISBNSearch from "./components/ISBNSearch";
 const Home = () => {
 	return (
 		<div className="bg-yellow-50 flex h-screen items-center justify-center w-screen">
-			<ISBNSearch buttonColor="bg-[#87A96B]"/>
+			<ISBNSearch buttonColor="bg-[#87A96B] hover:bg-[#7B9A61]" />
 		</div>
 	);
 };
